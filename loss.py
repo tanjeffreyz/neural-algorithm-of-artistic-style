eec66b096473a912ca88e5da4e2d098fe44329f1
Jeffrey Tan <tanjeffreyz02@berkeley.edu>
2023-05-09 14:41:12 -0700

    tuned constant in style loss

--- loss.py
+++ loss.py
@@ -6,22 +6,30 @@ def content_loss(p: torch.Tensor, x: torch.Tensor):
     return torch.sum((p - x) ** 2) / 2
 
 
-def _layer_style_loss(a, x):
+def _layer_style_loss(a: torch.Tensor, x: torch.Tensor):
     a_flat = torch.flatten(a, start_dim=2)
-    _, n, m = a_flat.shape
-
     x_flat = torch.flatten(x, start_dim=2)
+    b, n, m = a_flat.shape
+
     a_gram = torch.matmul(a_flat, torch.transpose(a_flat, 1, 2))
     x_gram = torch.matmul(x_flat, torch.transpose(x_flat, 1, 2))
-    return torch.sum((a_gram - x_gram) ** 2) / (4 * (n ** 2) * (m ** 2))
+    return torch.sum((a_gram - x_gram) ** 2) / (b * n * m)
 
 
 def style_loss(a_list, x_list):
     assert len(a_list) == len(x_list), 'Number of original and generated feature maps must be equal'
-    n = len(a_list)
+    w_l = 1 / len(a_list)
+
+    result = torch.zeros([])
+    for a, x in zip(a_list, x_list):
+        result += _layer_style_loss(a, x) * w_l
+    return result
 
 
-test1 = torch.rand((64, 16, 32, 32))
-test2 = torch.rand((64, 16, 32, 32))
-
-print(_layer_style_loss(test1, test2).shape)
+if __name__ == '__main__':
+    shape = (1, 16, 32, 32)
+    test1 = torch.rand(shape)
+    test2 = torch.rand(shape)
+    print(content_loss(test1, test2).item())
+    print(_layer_style_loss(test1, test2).item())
+    print(style_loss([test1, test1, test1], [test2, test2, test2]).item())
