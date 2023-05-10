149f8d79ee3d6756317a295916c0977daf417fea
Jeffrey Tan <tanjeffreyz02@berkeley.edu>
2023-05-10 14:47:03 -0700

    removed scale factor from saved config files

--- output/05_10_2023/14_33_16/config.py
+++ output/05_10_2023/14_33_16/config.py
@@ -5,7 +5,6 @@ DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 CONTENT_IMAGE = 'tubingen.jpg'
 STYLE_IMAGE = 'picasso.jpg'
-SCALE_FACTOR = 0.5          # Scales both dimensions of the content image
 
 CONTENT_LAYERS = ['conv_4']
 STYLE_LAYERS = ['conv_1', 'conv_2', 'conv_3', 'conv_4', 'conv_5']
