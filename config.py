4decaf67d4120ad4b955802850362fe520ef278f
Jeffrey Tan <tanjeffreyz02@berkeley.edu>
2023-05-09 23:41:03 -0700

    model built correctly, resizing style image to be same dims as content image

--- config.py
+++ config.py
@@ -6,6 +6,9 @@ DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 CONTENT_IMAGE = ''
 STYLE_IMAGE = ''
 
+CONTENT_LAYERS = ['conv_4']
+STYLE_LAYERS = ['conv_1', 'conv_2', 'conv_3', 'conv_4', 'conv_5']
+
 CONTENT_WEIGHT = 1E-3
 STYLE_WEIGHT = 1
 
--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 import torch.nn as nn
+import torchvision.transforms.functional
 from torchvision.models import vgg19
-from config import *
+from config import DEVICE
 
 
 def get_gram_matrix(features):
@@ -48,12 +49,26 @@ class StyleLoss(nn.Module):
 
 
 class NeuralStyleTransfer(nn.Module):
-    def __init__(self):
+    def __init__(self,
+                 content_img, style_img,
+                 content_layers, style_layers):
         super().__init__()
 
+        assert len(content_img.shape) == 4, 'Content image tensor must be 4-dimensional: BxCxWxH'
+        assert len(style_img.shape) == 4, 'Style image tensor must be 4-dimensional: BxCxWxH'
+
+        # Resize style image to be same size as content image
+        style_img = torchvision.transforms.functional.resize(style_img, content_img.shape[-2:])
+        style_img = style_img.to(DEVICE)
+        content_img = content_img.to(DEVICE)
+
+        # Build the model based on PyTorch's pretrained VGG-19
         vgg_model = vgg19(pretrained=True)
         vgg_layers = vgg_model.features.to(DEVICE).eval()
-        self.model = nn.Sequential(Normalize())
+        self.model = nn.Sequential(Normalize().to(DEVICE))
+
+        self.content_loss_layers = []
+        self.style_loss_layers = []
 
         i = 0
         for layer in vgg_layers.children():
@@ -64,19 +79,36 @@ class NeuralStyleTransfer(nn.Module):
                 name = f'bn_{i}'
             elif isinstance(layer, nn.ReLU):
                 name = f'relu_{i}'
-                layer = nn.ReLU(inplace=False)
+                layer = nn.ReLU(inplace=False).to(DEVICE)
             elif isinstance(layer, nn.MaxPool2d):
                 name = f'avgpool_{i}'
                 layer = nn.AvgPool2d(       # Paper said avg pool produced better results
                     kernel_size=layer.kernel_size,
                     stride=layer.stride,
                     padding=layer.padding
-                )
+                ).to(DEVICE)
             else:
-                continue
+                raise NotImplementedError('This case should never have been reached')
 
+            self.model.add_module(name, layer)
 
+            # Add loss layers after specified layers
+            if name in content_layers:
+                content_features = self.model(content_img)
+                content_loss_layer = ContentLoss(content_features).to(DEVICE)
+                self.model.add_module(f'content_loss_{i}', content_loss_layer)
+                self.content_loss_layers.append(content_loss_layer)
+            if name in style_layers:
+                style_features = self.model(style_img)
+                style_loss_layer = StyleLoss(style_features).to(DEVICE)
+                self.model.add_module(f'style_loss_{i}', style_loss_layer)
+                self.style_loss_layers.append(style_loss_layer)
 
+        # Trim model until deepest loss layer to avoid unnecessary computation
+        for i in reversed(range(len(self.model))):
+            if isinstance(self.model[i], ContentLoss) or isinstance(self.model[i], StyleLoss):
+                break
+        self.model = self.model[:i+1]
 
     def forward(self, x):
         return self.model.forward(x)
@@ -84,8 +116,24 @@ class NeuralStyleTransfer(nn.Module):
 
 if __name__ == '__main__':
     import torch
+    from config import CONTENT_LAYERS, STYLE_LAYERS
 
-    # Check that dimensions are correct
-    test = torch.rand((1, 16, 32, 32))
-    ContentLoss(test).forward(test)
-    StyleLoss(test).forward(test)
+    # Check that loss layer dimensions are correct
+    test1 = torch.randn(1, 16, 32, 32)
+    test2 = torch.randn(1, 16, 32, 32)
+
+    print(get_gram_matrix(test1).shape)
+
+    cl = ContentLoss(test1)
+    cl.forward(test2)
+    print(cl.loss)
+
+    sl = StyleLoss(test1)
+    sl.forward(test2)
+    print(sl.loss)
+
+    # Check that model was built correctly
+    content_test = torch.randn(1, 3, 1920, 1080)
+    style_test = torch.randn(1, 3, 1234, 1234)
+    model = NeuralStyleTransfer(content_test, style_test, CONTENT_LAYERS, STYLE_LAYERS)
+    print(model)
